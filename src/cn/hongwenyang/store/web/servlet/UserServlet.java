2ffe56c394f16fdc0e38f0fdadb429af4e38ccd1
hongwenyang <13858126467@163.com>
2018-10-09 23:23:42 +0800

    修改了UserServlet

--- src/cn/hongwenyang/store/web/servlet/UserServlet.java
+++ src/cn/hongwenyang/store/web/servlet/UserServlet.java
@@ -1,7 +1,11 @@
 package cn.hongwenyang.store.web.servlet;
 
+import cn.hongwenyang.store.domain.User;
+import cn.hongwenyang.store.utils.MyBeanUtils;
 import cn.hongwenyang.store.web.base.BaseServlet;
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +14,19 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class UserServlet
  */
 public class UserServlet extends BaseServlet {
+	private Map<String, String[]> parameterMap;
+	private User populate;
+
+	public String registerUI(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		return "/jsp/register.jsp";
+	}
+	
+	
+	public String userRegist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		parameterMap = request.getParameterMap();
+		populate = MyBeanUtils.populate(User.class, parameterMap);
+		return "/jsp/register.jsp";
+	}
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 	}
